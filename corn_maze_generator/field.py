27b0f38ac3b522a1ffc29e32c1a08926c8375e97
Alexander Baker <alexebaker@alexebaker.com>
2018-04-06 20:38:37 -0600

    started bfs

--- corn_maze_generator/field.py
+++ corn_maze_generator/field.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,8 +6,8 @@ from numpy.random import random_integers as randint
 from queue import Queue
 
 
-ACRE_WIDTH_FT = 208.71
-ACRE_HEIGHT_FT = 208.71
+ACRE_WIDTH_FT = math.sqrt(43560)
+ACRE_HEIGHT_FT = ACRE_WIDTH_FT
 
 
 class Field:
@@ -34,19 +35,19 @@ class Field:
         self._field[:, 0] = 1
         self._field[:, -1] = 1
 
-        for i in range(self.density):
+        for _ in range(self.density):
             x, y = randint(0, shape[1] // 2) * 2, randint(0, shape[0] // 2) * 2
             self._field[y, x] = 1
-            for j in range(self.complexity):
+            for _ in range(self.complexity):
                 neighbors = []
                 if x > 1:
-                    neighbors.append((y, x - 2))
+                    neighbors.append((y, x-2))
                 if x < shape[1] - 2:
-                    neighbors.append((y, x + 2))
+                    neighbors.append((y, x+2))
                 if y > 1:
-                    neighbors.append((y - 2, x))
+                    neighbors.append((y-2, x))
                 if y < shape[0] - 2:
-                    neighbors.append((y + 2, x))
+                    neighbors.append((y+2, x))
                 if neighbors:
                     y_, x_ = neighbors[randint(0, len(neighbors) - 1)]
                     if self._field[y_, x_] == 0:
@@ -60,7 +61,8 @@ class Field:
     def finish(self):
         shape = self._field.shape
         tmp = np.where(self._field == 1)
-        start_points = [(r, c) for (r, c) in zip(tmp[0], tmp[1]) if ((r == 0 or r == shape[0]-1) and (c == 0 or c == shape[1]-1))]
+        start_points = [(r, c) for (r, c) in zip(tmp[0], tmp[1])
+                        if ((r == 0 and c < shape[1]-1) or (c == 0 and r < shape[0]-1))]
 
         for p in start_points:
             queue = Queue()
@@ -80,19 +82,17 @@ class Field:
                         queue.put(neighbor)
 
                 visited[root] = 1
-
-
         return
 
     def _get_neighbors(self, idx):
         neighbors = []
         row, col = idx
         if row-1 >= 0:
-            neighbors.append(row-1, col)
+            neighbors.append((row-1, col))
         if row+1 < self._field.shape[0]:
-            neighbors.append(row+1, col)
+            neighbors.append((row+1, col))
         if col-1 >= 0:
-            neighbors.append(row, col-1)
+            neighbors.append((row, col-1))
         if col+1 < self._field.shape[1]:
-            neighbors.append(row, col+1)
+            neighbors.append((row, col+1))
         return [n for n in neighbors if not self._field[n]]
