fceace0683e270d74ccc3b57f696c30958cdb688
Alexander Baker <alexebaker@alexebaker.com>
2018-04-06 19:29:43 -0600

    started bfs

--- corn_maze_generator/field.py
+++ corn_maze_generator/field.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from numpy.random import random_integers as randint
+from queue import Queue
 
 
 ACRE_WIDTH_FT = 208.71
@@ -52,4 +53,46 @@ class Field:
                         self._field[y_, x_] = 1
                         self._field[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
                         x, y = x_, y_
+
+        self.finish()
         return
+
+    def finish(self):
+        shape = self._field.shape
+        tmp = np.where(self._field == 1)
+        start_points = [(r, c) for (r, c) in zip(tmp[0], tmp[1]) if ((r == 0 or r == shape[0]-1) and (c == 0 or c == shape[1]-1))]
+
+        for p in start_points:
+            queue = Queue()
+            meta = {}
+            visited = np.zeros(self._field.shape, dtype=bool)
+            queue.put(p)
+
+            while not queue.empty():
+                root = queue.get()
+
+                for neighbor in self._get_neighbors(root):
+                    if visited[neighbor]:
+                        continue
+
+                    if neighbor not in queue:
+                        meta[neighbor] = root
+                        queue.put(neighbor)
+
+                visited[root] = 1
+
+
+        return
+
+    def _get_neighbors(self, idx):
+        neighbors = []
+        row, col = idx
+        if row-1 >= 0:
+            neighbors.append(row-1, col)
+        if row+1 < self._field.shape[0]:
+            neighbors.append(row+1, col)
+        if col-1 >= 0:
+            neighbors.append(row, col-1)
+        if col+1 < self._field.shape[1]:
+            neighbors.append(row, col+1)
+        return [n for n in neighbors if not self._field[n]]
