bc9b60dd042e853a3b4cbe3838c16888a82a1afb
Alexander Baker <alexebaker@alexebaker.com>
2018-07-19 00:25:15 -0600

    working solution finder

--- corn_maze_generator/field.py
+++ corn_maze_generator/field.py
@@ -15,12 +15,21 @@ class Field:
     def __init__(self, size=(4, 2), diameter=10, complexity=0.75, density=0.75):
         height = (size[0] * ACRE_HEIGHT_FT) // diameter
         width = (size[1] * ACRE_WIDTH_FT) // diameter
-        shape = (int((height // 2) * 2 + 1), int((width // 2) * 2 + 1))
+        shape = (int((height // 2) * 2 + 1) + 2, int((width // 2) * 2 + 1) + 2)
 
         self.complexity = int(complexity * (5 * (shape[0] + shape[1])))
         self.density = int(density * ((shape[0] // 2) * (shape[1] // 2)))
         self._field = np.zeros(shape, dtype=bool)
         self._solution = []
+
+        edge_points = []
+        for row in range(0, self._field.shape[0]):
+            edge_points.append((row, 0))
+            edge_points.append((row, self._field.shape[1]-1))
+        for col in range(0, self._field.shape[1]):
+            edge_points.append((0, col))
+            edge_points.append((self._field.shape[0]-1, col))
+        self.edge_points = edge_points
         return
 
     def display(self):
@@ -60,17 +69,23 @@ class Field:
                         self._field[y_, x_] = 1
                         self._field[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
                         x, y = x_, y_
+
+        edge_points = []
+        for row in range(1, self._field.shape[0]-1):
+            edge_points.append((row, 1))
+            edge_points.append((row, self._field.shape[1]-2))
+        for col in range(1, self._field.shape[1]):
+            edge_points.append((1, col))
+            edge_points.append((self._field.shape[0]-2, col))
+        for point in edge_points:
+            if not self._field[point]:
+                self._field[point] = True
+                if not self._is_connected():
+                    self._field[point] = False
         return
 
     def find_solution(self):
-        shape = self._field.shape
-        tmp = np.where(self._field == 1)
-        start_points = [
-            (r, c) for (r, c) in zip(tmp[0], tmp[1])
-            if ((r == 0 and c < shape[1]-1) or (c == 0 and r < shape[0]-1) or
-                (r == shape[0]-1 and c > 0) or (c == shape[1]-1 and r > 0))]
-
-        for p in start_points:
+        for p in self.edge_points:
             queue = BFSQueue()
             meta = {p: None}
             visited = np.zeros(self._field.shape, dtype=bool)
@@ -80,18 +95,20 @@ class Field:
                 root = queue.get()
 
                 for neighbor in self._get_neighbors(root):
-                    if visited[neighbor]:
-                        continue
+                    if not self._field[neighbor]:
+                        if visited[neighbor]:
+                            continue
 
-                    if neighbor not in queue:
-                        meta[neighbor] = root
-                        queue.put(neighbor)
-                        new_solution = self._get_solution(neighbor, meta)
+                        if neighbor not in queue:
+                            meta[neighbor] = root
+                            queue.put(neighbor)
+                    elif neighbor in self.edge_points and root not in self.edge_points:
+                        new_solution = self._get_solution(root, meta)
+                        new_solution.append(neighbor)
                         if len(new_solution) > len(self._solution):
                             self._solution = new_solution
 
                 visited[root] = True
-
         return
 
     def _get_solution(self, point, meta):
@@ -100,6 +117,25 @@ class Field:
             solution.append(point)
             point = meta[point]
         return solution
+
+    def _is_connected(self):
+        queue = BFSQueue()
+        start_point = np.where(self._field == 0)[0][0], np.where(self._field == 0)[1][0]
+        visited = np.ones(self._field.shape, dtype=bool)
+        queue.put(start_point)
+
+        while not queue.empty():
+            root = queue.get()
+
+            for neighbor in self._get_neighbors(root):
+                if not self._field[neighbor]:
+                    if not visited[neighbor]:
+                        continue
+
+                    if neighbor not in queue:
+                        queue.put(neighbor)
+            visited[root] = False
+        return np.array_equal(self._field, visited)
 
     def _get_neighbors(self, point):
         neighbors = []
@@ -112,7 +148,7 @@ class Field:
             neighbors.append((row, col-1))
         if col+1 < self._field.shape[1]:
             neighbors.append((row, col+1))
-        return [n for n in neighbors if not self._field[n]]
+        return neighbors
 
 
 class BFSQueue:
